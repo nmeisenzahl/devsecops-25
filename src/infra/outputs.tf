518efc2837bc3fe6d45433983a159c1f2711f5e0
Nico Meisenzahl <nico@meisenzahl.org>
2025-05-17 22:41:14 +0200

    feat: adds app cicd

--- src/infra/outputs.tf
+++ src/infra/outputs.tf
@@ -1,1 +1,5 @@
 # filepath: src/infra/outputs.tf
+output "gh_action_uai_client_id" {
+  description = "User Assigned Identity ID for GitHub Actions"
+  value       = azurerm_user_assigned_identity.github_actions.client_id
+}
