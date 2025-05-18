d9c5af7911563e29b97a173cb467eee0ae4ed3fa
Nico Meisenzahl <nico@meisenzahl.org>
2025-05-18 12:43:28 +0200

    feat: infra updates (#6)

--- src/infra/main.tf
+++ src/infra/main.tf
@@ -1,16 +1,20 @@
+// Resource Group to contain all resources
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-rg"
   location = var.location
 }
 
+// Retrieve current client configuration (tenant and subscription info)
 data "azurerm_client_config" "current" {}
 
+// User-assigned identity for the Container App
 resource "azurerm_user_assigned_identity" "app_identity" {
   name                = "${var.prefix}-uai"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+// Log Analytics Workspace for Container App logs
 resource "azurerm_log_analytics_workspace" "law" {
   name                = "${var.prefix}-law"
   resource_group_name = azurerm_resource_group.rg.name
@@ -20,14 +24,43 @@ resource "azurerm_log_analytics_workspace" "law" {
   retention_in_days   = 30
 }
 
+// Virtual Network for network isolation
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.prefix}-vnet"
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+// Subnet for Container App Environment with private endpoint policies disabled
+resource "azurerm_subnet" "app_env_subnet" {
+  name                                          = "app-env"
+  resource_group_name                           = azurerm_resource_group.rg.name
+  virtual_network_name                          = azurerm_virtual_network.vnet.name
+  address_prefixes                              = [var.subnet_app]
+  private_link_service_network_policies_enabled = true
+}
+
+// Subnet for SQL Private Endpoint to connect securely to SQL Server
+resource "azurerm_subnet" "pe_subnet" {
+  name                                          = "sql-pe"
+  resource_group_name                           = azurerm_resource_group.rg.name
+  virtual_network_name                          = azurerm_virtual_network.vnet.name
+  address_prefixes                              = [var.subnet_pe]
+  private_link_service_network_policies_enabled = true
+}
+
+// Container App Environment integrated with VNet and Log Analytics
 resource "azurerm_container_app_environment" "env" {
   name                       = "${var.prefix}-env"
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = azurerm_resource_group.rg.location
   logs_destination           = "log-analytics"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  infrastructure_subnet_id   = azurerm_subnet.app_env_subnet.id
 }
 
+// Container App running the Go API
 resource "azurerm_container_app" "app" {
   name                         = "${var.prefix}-app"
   container_app_environment_id = azurerm_container_app_environment.env.id
@@ -83,12 +116,14 @@ resource "azurerm_container_app" "app" {
   }
 }
 
+// Azure SQL Server with Managed Identity as AD admin
 resource "azurerm_mssql_server" "sql" {
-  name                = "${var.prefix}-sql"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  version             = "12.0"
-  minimum_tls_version = "1.2"
+  name                          = "${var.prefix}-sql"
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
+  version                       = "12.0"
+  minimum_tls_version           = "1.2"
+  public_network_access_enabled = false
 
   # configure Azure AD admin using the user-assigned identity
   azuread_administrator {
@@ -99,28 +134,60 @@ resource "azurerm_mssql_server" "sql" {
   }
 }
 
+// Azure SQL Database for application data
 resource "azurerm_mssql_database" "db" {
   name      = "${var.prefix}-db"
   server_id = azurerm_mssql_server.sql.id
   sku_name  = "S0"
 }
 
-resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
-  server_id        = azurerm_mssql_server.sql.id
-  name             = "AllowAll"
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
+// Private Endpoint for SQL Server to allow private connectivity
+resource "azurerm_private_endpoint" "sql_pe" {
+  name                = "${var.prefix}-sql-pe"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  subnet_id           = azurerm_subnet.pe_subnet.id
+
+  private_service_connection {
+    name                           = "sql-psc"
+    private_connection_resource_id = azurerm_mssql_server.sql.id
+    subresource_names              = ["sqlServer"]
+    is_manual_connection           = false
+  }
 }
 
-# UAI for GitHub Action Workflow
+// Private DNS Zone for SQL Private Endpoint resolution
+resource "azurerm_private_dns_zone" "sql_zone" {
+  name                = "privatelink.database.windows.net"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+// Link the Private DNS Zone to the VNet
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
+  name                  = "link-to-vnet"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.sql_zone.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+// A record for SQL Private Endpoint to resolve server privately
+resource "azurerm_private_dns_a_record" "sql_pe_record" {
+  name                = azurerm_mssql_server.sql.name
+  zone_name           = azurerm_private_dns_zone.sql_zone.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.sql_pe.private_service_connection[0].private_ip_address]
+}
+
+// User-assigned identity for GitHub Actions with federated credentials
 resource "azurerm_user_assigned_identity" "github_actions" {
   name                = "${var.prefix}-uai-gh"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
+// Federated Identity Credential for GitHub Actions
 resource "azurerm_federated_identity_credential" "github_actions" {
-
   name                = "${var.prefix}-fedcrd-gh"
   resource_group_name = azurerm_resource_group.rg.name
   parent_id           = azurerm_user_assigned_identity.github_actions.id
@@ -129,9 +196,9 @@ resource "azurerm_federated_identity_credential" "github_actions" {
   subject             = var.oidc_subject
 }
 
+// Role Assignment for GitHub Actions identity in Resource Group
 resource "azurerm_role_assignment" "github_actions" {
   scope                = azurerm_resource_group.rg.id
   role_definition_name = "Contributor"
   principal_id         = azurerm_user_assigned_identity.github_actions.principal_id
-
 }
--- src/infra/variables.tf
+++ src/infra/variables.tf
@@ -23,3 +23,21 @@ variable "oidc_subject" {
   type        = string
   default     = "repo:nmeisenzahl/devsecops-25:ref:refs/heads/main"
 }
+
+variable "vnet_address_space" {
+  description = "Address space for the virtual network"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_app" {
+  description = "Subnet for Container App Environment"
+  type        = string
+  default     = "10.0.0.0/23"
+}
+
+variable "subnet_pe" {
+  description = "Subnet for Private Endpoint"
+  type        = string
+  default     = "10.0.2.0/24"
+}
