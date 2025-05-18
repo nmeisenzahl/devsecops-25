134fed81c0e66b05bc93912dc156048319a0243a
Nico Meisenzahl <nico@meisenzahl.org>
2025-05-18 12:42:48 +0200

    feat: infra updates

--- src/infra/outputs.tf
+++ src/infra/outputs.tf
@@ -3,3 +3,15 @@ output "gh_action_uai_client_id" {
   description = "User Assigned Identity ID for GitHub Actions"
   value       = azurerm_user_assigned_identity.github_actions.client_id
 }
+
+output "sql_server_name" {
+  description = "SQL Server Name"
+  value       = azurerm_mssql_server.sql.name
+  
+}
+
+output "sql_database_name" {
+  description = "SQL Database Name"
+  value       = azurerm_mssql_database.db.name
+  
+}
